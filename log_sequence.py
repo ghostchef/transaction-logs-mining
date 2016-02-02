7523d8c1636b07bc31668f61acf5b90db8450626
ghostchef <chuang1159@foxmail.com>
2016-02-02 20:22:59 +0800

    log sequence sorted by timestamp

--- log_sequence.py
+++ log_sequence.py
@@ -1,6 +1,8 @@
 #-*- coding: utf-8 -*-
 import os
 import sys
+import time
+from operator import itemgetter
 
 reload(sys)
 sys.setdefaultencoding( "utf-8" )
@@ -20,6 +22,11 @@ for file in files:
             record = line.split()
             type = 0
 
+            log_time = record[5] + " " + record[6]
+            timeArray = time.strptime(log_time,'%Y-%m-%d %H:%M:%S')
+            log_time_stamp = int(time.mktime(timeArray))
+            new_record = record[1], record[2], record[3], record[4], log_time_stamp
+
             for word in sp_list:
                 if word in record[4]:
                     type = 1
@@ -30,16 +37,18 @@ for file in files:
                 group_ID = record[3] + '_' + record[9]
 
             if group_ID in group:
-                group[group_ID].append(line)
+                group[group_ID].append(new_record)
             else:
                 group[group_ID] = []
-                group[group_ID].append(line)
+                group[group_ID].append(new_record)
 
         cur_file.close()
 
 for id, sequence in group.iteritems():
     outfile = open(data_src+"group/group_" + str(group_index) + ".txt", 'w')
     group_index += 1
-    for log in sequence:
-        outfile.write(log)
+    sorted_log = sorted(sequence, key=itemgetter(4))
+
+    for log in sorted_log:
+        outfile.write(log[0] + ' ' + log[1] + ' '+ log[2] + ' ' + log[3] + ' ' + str(log[4]) + '\n')
     outfile.close()
--- python_test.py
+++ python_test.py
@@ -1,22 +1,37 @@
 #-*- coding: utf-8 -*-
+# some test code for python feature
 import os
 import sys
+import time
 
 reload(sys)
 sys.setdefaultencoding( "utf-8" )
-
 data_src = "/Users/ghostchef/data/shcool-card-data/"
 
-sp_list = ["开水房", "浴室"]
+log_time = "2013-10-10 23:40:00"
 
-# line = "启秀开水房"
-# line = "南山浴室"
-line = "中华食堂"
+timeArray = time.strptime(log_time,'%Y-%m-%d %H:%M:%S')
+print timeArray
+log_time_stamp = int(time.mktime(timeArray))
+print log_time_stamp
 
-type = 0
+timeArray_2 = time.localtime(log_time_stamp)
+print timeArray_2
+otherStyleTime = time.strftime("%Y-%m-%d %H:%M:%S", timeArray_2)
+print otherStyleTime
 
-for word in sp_list:
-    if word in line:
-        type = 1
 
-print type
+
+# sp_list = ["开水房", "浴室"]
+#
+# # line = "启秀开水房"
+# # line = "南山浴室"
+# line = "中华食堂"
+#
+# type = 0
+#
+# for word in sp_list:
+#     if word in line:
+#         type = 1
+#
+# print type
