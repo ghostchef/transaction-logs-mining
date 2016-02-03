05ce910b9d145392ca13c14bc8f3dd778185e627
ghostchef <chuang1159@foxmail.com>
2016-02-03 16:30:06 +0800

    refactor profile process

--- user_profile_process.py
+++ user_profile_process.py
@@ -7,32 +7,80 @@ reload(sys)
 sys.setdefaultencoding( "utf-8" )
 data_src = "/Users/ghostchef/data/shcool-card-data/"
 path = data_src + "cleaned-logs/"
-# path = data_src + "original-logs/"
 
 files = os.listdir(path)
 exclude_list = ['.', 'x']
 
+
+# process student profile: count student number and collect names
+prof_name_dict = dict()
+class_dict = dict()
+major_dict = dict()
+shcool_dict = dict()
+prof_file = data_src + "original-logs/xs_ykt.xls"
+data = xlrd.open_workbook(prof_file)
+table = data.sheets()[0]
+for i in range(1, table.nrows):
+    row = table.row_values(i)
+    # encode to utf-8 before add to set
+    # add record to name dict
+    if prof_name_dict.has_key(unicode(row[5]).encode('utf-8')):
+        pass
+    else:
+        prof_name_dict[unicode(row[5]).encode('utf-8')] = unicode(row[6]).encode('utf-8')
+
+    # add record to class dict
+    if class_dict.has_key(unicode(row[3]).encode('utf-8')):
+        class_dict[unicode(row[3]).encode('utf-8')].add(unicode(row[6]).encode('utf-8'))
+    else:
+        class_dict[unicode(row[3]).encode('utf-8')] = {unicode(row[6]).encode('utf-8')}
+
+    # add record to major dict
+    if major_dict.has_key(unicode(row[7]).encode('utf-8')):
+        major_dict[unicode(row[7]).encode('utf-8')].add(unicode(row[6]).encode('utf-8'))
+    else:
+        major_dict[unicode(row[7]).encode('utf-8')] = {unicode(row[6]).encode('utf-8')}
+
+    # add record to school dict
+    if shcool_dict.has_key(unicode(row[1]).encode('utf-8')):
+        shcool_dict[unicode(row[1]).encode('utf-8')].add(unicode(row[6]).encode('utf-8'))
+    else:
+        shcool_dict[unicode(row[1]).encode('utf-8')] = {unicode(row[6]).encode('utf-8')}
+
+# write profile info into files
+prof_name_file = open(data_src+'prof_stuID_name.txt', 'w')
+for k, v in prof_name_dict.iteritems():
+    prof_name_file.write(k + ":" + v + '\n')
+prof_name_file.close()
+
+class_name_file = open(data_src+'class_member.txt', 'w')
+for k, v in class_dict.iteritems():
+    class_name_file.write(k + ":{")
+    for name in v:
+        class_name_file.write(name+', ')
+    class_name_file.write("}\n")
+class_name_file.close()
+
+major_name_file = open(data_src+'major_member.txt', 'w')
+for k, v in major_dict.iteritems():
+    major_name_file.write(k + ":{")
+    for name in v:
+        major_name_file.write(name+', ')
+    major_name_file.write("}\n")
+major_name_file.close()
+
+shcool_name_file = open(data_src+'school_member.txt', 'w')
+for k, v in shcool_dict.iteritems():
+    shcool_name_file.write(k + ":{")
+    for name in v:
+        shcool_name_file.write(name+', ')
+    shcool_name_file.write("}\n")
+shcool_name_file.close()
+
+
+
+# process log files: count consumer numbers and build ID-name dict
 log_name_dict = dict()
-log_name_set = set([])
-prof_name_set = set([])
-
-prof_stu_count = 0
-log_stu_count = 0
-log_count = 0
-
-
-# # process student profile: count student number and collect names
-# stu_file = data_src + "original-logs/xs_ykt.xls"
-# data = xlrd.open_workbook(stu_file)
-# table = data.sheets()[0]
-# stu_count = table.nrows - 1
-# for i in range(1, table.nrows):
-#     row = table.row_values(i)
-#     # encode to utf-8 before add to set
-#     prof_name_set.add(unicode(row[6]).encode('utf-8'))
-
-
-# process log files: count consumer numbers and collect names and ID
 for file in files:
     cur_file = open(path+file, 'r')
     for line in cur_file.readlines():
@@ -41,53 +89,21 @@ for file in files:
             pass
         else:
             log_name_dict[record[1]] = record[2]
-        # log_name_set.add(record[2])
     cur_file.close()
 
 log_name_file = open(data_src+'log_ID_name.txt', 'w')
 for k, v in log_name_dict.iteritems():
     log_name_file.write(k + ":" + v + '\n')
 
-# log_stu_count = len(log_name_dict)
-# name_combine_set = log_name_set & prof_name_set
-#
-# print "profile student count: " + str(prof_stu_count)
-# print "logs student count: " + str(log_stu_count)
-# print "profile student name count: " + str(len(prof_name_set))
-# print "logs student name count: " + str(len(log_name_set))
-# print "combine name count: " + str(len(name_combine_set))
-#
-# profile_names = open(data_src+"profile_names.txt", 'w')
-# log_names = open(data_src+ "log_names.txt", 'w')
-# combine_names = open(data_src+ "combine_names.txt", 'w')
-#
-# for name in prof_name_set:
-#     profile_names.write(str(name)+'\n')
-# for name in log_name_set:
-#     log_names.write(str(name)+'\n')
-# for name in name_combine_set:
-#     combine_names.write(str(name)+'\n')
 
 
-# # count log numbers
-# for f in files:
-#     if f[0] not in exclude_list:
-#         curr_file = open(path + f, 'r')
-#         count = len(curr_file.readlines())
-#         log_count += count - 1
-#         curr_file.close()
-#
-# print log_count
+# count log numbers
+log_count = 0
+for f in files:
+    if f[0] not in exclude_list:
+        curr_file = open(path + f, 'r')
+        count = len(curr_file.readlines())
+        log_count += count
+        curr_file.close()
 
-
-
-
-
-
-
-
-
-
-
-
-
+print log_count
