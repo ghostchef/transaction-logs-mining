4b5487db69ba1d5d452d13dca5273c29c1ea8a30
ghostchef <chuang1159@foxmail.com>
2016-02-06 13:43:24 +0800

    refactor cool weight

--- cooc_weight.py
+++ cooc_weight.py
@@ -60,44 +60,51 @@ for fi in files:
             p_ul[(user, loc)] = times / v_u[user]
 
 
-        # compute p_lu: the probability of a location visited by a user among other users
-        for user, times in v_ul.iteritems():
-            p_lu[user] = times / v_l
-
-        # compute entropy
-        for k, v in p_lu.iteritems():
-            h_l += (v * math.log(v))
-
-        # compute weight of location
-        w_l = math.exp(h_l)
-        loc_weight[loc] = w_l
-
-# normalize location weight and write into files
-for k, v in loc_weight.iteritems():
-    nom_loc_weight[k] = 100 * v
-
-loc_weight_file = open(data_src+'loc_weight.txt', 'w')
-for k, v in loc_weight.iteritems():
-    loc_weight_file.write(k+','+str(v)+'\n')
-loc_weight_file.close()
-
-nor_loc_weight_file = open(data_src+'nom_loc_weight.txt', 'w')
-for k, v in nom_loc_weight.iteritems():
-    nor_loc_weight_file.write(k+','+str(v)+'\n')
-nor_loc_weight_file.close()
-
-# copy list
-pul_list_1 = []
-pul_list_2 = []
+pul_file = open(data_src + 'pul.txt', 'w')
 for k, v in p_ul.iteritems():
-    pul_list_1.append((k, v))
-    pul_list_2.append((k, v))
+    pul_file.write(str(k) + ':' + str(v) + '\n')
 
-# computing w_uvl"
-per_weight_file = open(data_src+'personal_weight.txt', 'w')
-for i in range(0, len(pul_list_1)):
-    for j in range(i+1, len(pul_list_2)):
-        if pul_list_1[i][0][1] ==  pul_list_2[j][0][1] and pul_list_1[i][0][0] != pul_list_2[j][0][0]:
-            w_uvl_v = -1 * math.log(pul_list_1[i][1] * pul_list_2[j][1])
-            per_weight_file.write(str((pul_list_1[i][0][0], pul_list_2[j][0][0], pul_list_2[j][0][1]))+ ':' + str(w_uvl_v) + '\n')
-per_weight_file.close()
+
+        # # compute p_lu: the probability of a location visited by a user among other users
+        # for user, times in v_ul.iteritems():
+        #     p_lu[user] = times / v_l
+        #
+        # # compute entropy
+        # for k, v in p_lu.iteritems():
+        #     h_l += (v * math.log(v))
+        #
+        # # compute weight of location
+        # w_l = math.exp(h_l)
+        # loc_weight[loc] = w_l
+
+# # normalize location weight and write into files
+# for k, v in loc_weight.iteritems():
+#     nom_loc_weight[k] = 100 * v
+#
+# loc_weight_file = open(data_src+'loc_weight.txt', 'w')
+# for k, v in loc_weight.iteritems():
+#     loc_weight_file.write(k+','+str(v)+'\n')
+# loc_weight_file.close()
+#
+# nor_loc_weight_file = open(data_src+'nom_loc_weight.txt', 'w')
+# for k, v in nom_loc_weight.iteritems():
+#     nor_loc_weight_file.write(k+','+str(v)+'\n')
+# nor_loc_weight_file.close()
+
+
+# # copy list
+# pul_list_1 = []
+# pul_list_2 = []
+# for k, v in p_ul.iteritems():
+#     pul_list_1.append((k, v))
+#     pul_list_2.append((k, v))
+#
+# # computing w_uvl"
+# # this step can never finish, even finished, it can't be use again
+# per_weight_file = open(data_src+'personal_weight.txt', 'w')
+# for i in range(0, len(pul_list_1)):
+#     for j in range(i+1, len(pul_list_2)):
+#         if pul_list_1[i][0][1] ==  pul_list_2[j][0][1] and pul_list_1[i][0][0] != pul_list_2[j][0][0]:
+#             w_uvl_v = -1 * math.log(pul_list_1[i][1] * pul_list_2[j][1])
+#             per_weight_file.write(str((pul_list_1[i][0][0], pul_list_2[j][0][0], pul_list_2[j][0][1]))+ ':' + str(w_uvl_v) + '\n')
+# per_weight_file.close()
