acc76a2f1ae2fe58b7a9a0a6ce1e6aecafacad2d
ghostchef <chuang1159@foxmail.com>
2016-02-17 22:38:47 +0800

    change to weighted diversity

--- cooc_diversity.py
+++ cooc_diversity.py
@@ -5,7 +5,8 @@ import os
 import math
 
 data_src = "/Users/ghostchef/data/shcool-card-data/"
-freq_file = open(data_src + 'strength/cooc-freq.txt', 'r')
+# freq_file = open(data_src + 'strength/cooc-freq.txt', 'r')
+freq_file = open(data_src + 'strength/weight_freq.txt', 'r')
 cooc_freq = dict()
 entropy = dict()
 
@@ -14,18 +15,29 @@ for line in freq_file.readlines():
     rec = line.split(':')
     info = eval(rec[0])
     tup = (info[0], info[1])
-    cooc_freq[tup] = int(rec[1])
+    # cooc_freq[tup] = int(rec[1])
+    cooc_freq[tup] = float(rec[1])
 freq_file.close()
 
+local_weight = dict()
+weight_file = open(data_src + 'cooc/norm_weight.txt', 'r')
+for line in weight_file.readlines():
+    rec = line.split(':')
+    local_weight[rec[0]] = float(rec[1])
+weight_file.close()
 
-outfile = open(data_src + 'strength/cooc_diversity.txt', 'w')
+
+# outfile = open(data_src + 'strength/cooc_diversity.txt', 'w')
+outfile = open(data_src + 'strength/weight_diversity.txt', 'w')
 freq_file = open(data_src + 'cooc/local-freq-3.txt', 'r')
 
 for line in freq_file.readlines():
     rec = line.split(':')
     info = eval(rec[0])
     tup = (info[0], info[1])
-    p_ij = int(rec[1]) / cooc_freq[tup]
+    # p_ij = int(rec[1]) / cooc_freq[tup]
+    p_ij = ( float(rec[1]) * local_weight[rec[0]]) / cooc_freq[tup]
+
     local_ent = p_ij * math.log(p_ij)
     if entropy.has_key(tup):
         entropy[tup] += local_ent
