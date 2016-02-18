578923369a1cabab790fa87b6496573a9df14a25
ghostchef <chuang1159@foxmail.com>
2016-02-18 23:40:14 +0800

    some change at co-class rate

--- co_classrate.py
+++ co_classrate.py
@@ -11,6 +11,7 @@ data_src = "/Users/ghostchef/data/shcool-card-data/"
 
 profile = dict()
 class_info = dict()
+stu_set = set()
 profile_file = open(data_src + 'profile/log_ID_name.txt', 'r')
 
 for line in profile_file.readlines():
@@ -24,6 +25,7 @@ table = data.sheets()[0]
 for i in range(1, table.nrows):
     row = table.row_values(i)
 
+    stu_set.add(unicode(row[6]).encode('utf-8'))
     if class_info.has_key(unicode(row[3]).encode('utf-8')):
         class_info[unicode(row[3]).encode('utf-8')].add(unicode(row[6]).encode('utf-8'))
     else:
@@ -34,55 +36,59 @@ for i in range(1, table.nrows):
 
 break_point = [200, 2000, 10000, 20000, 40000, 60000, 80000, 100000, 200000, 300000, 400000, 410340]
 
+
+path = data_src + "sorted_strength/"
+files = os.listdir(path)
+for f in files:
+    if f[0] != '.':
+        file = open(data_src + 'sorted_strength/' + f, 'r')
+        outfile = open(data_src + 'co-class/' +f, 'w')
+        print 'we are processing file ' + f
+        hit_num = 0
+        non_stu = 0
+        for i in range(0, 410340):
+            line = file.readline()
+            rec = line.split(':')
+            tup = eval(rec[0])
+            name_1 = profile[tup[0]]
+            name_2 = profile[tup[1]]
+
+            if name_1 in stu_set and name_2 in stu_set:
+                for k, v in class_info.iteritems():
+                    if name_1 in v and name_2 in v:
+                        hit_num += 1
+                        break
+            else:
+                non_stu += 1
+
+            for point in break_point:
+                if i == point - 1:
+                    rate = hit_num / (point-non_stu)
+                    outfile.write(str(point/2) + ':' + str(rate) + '\n')
+        file.close()
+        outfile.close()
+
+
+
 #
-# path = data_src + "sorted_strength/"
-# files = os.listdir(path)
-# for f in files:
-#     if f[0] != '.':
-#         file = open(data_src + 'sorted_strength/' + f, 'r')
-#         outfile = open(data_src + 'co-class/' +f, 'w')
-#         print 'we are processing file ' + f
-#         hit_num = 0
-#         for i in range(0, 410340):
-#             line = file.readline()
-#             rec = line.split(':')
-#             tup = eval(rec[0])
-#             name_1 = profile[tup[0]]
-#             name_2 = profile[tup[1]]
+# # random guess co-class rate
+# print "we are computing co-class rate"
+# net_file = open(data_src + 'strength/random_pair.txt', 'r')
+# outfile = open(data_src + 'co-class/random_pair.txt', 'w')
 #
-#             for k, v in class_info.iteritems():
-#                 if name_1 in v and name_2 in v:
-#                     hit_num += 1
-#                     break
+# hit_num = 0
 #
-#             for point in break_point:
-#                 if i == point - 1:
-#                     rate = hit_num / point
-#                     outfile.write(str(point/2) + ':' + str(rate) + '\n')
-#         file.close()
-#         outfile.close()
-
-
-
-
-# random guess co-class rate
-print "we are computing co-class rate"
-net_file = open(data_src + 'strength/random_pair.txt', 'r')
-outfile = open(data_src + 'co-class/random_pair.txt', 'w')
-
-hit_num = 0
-
-for i in range(0, 205170):
-    line = net_file.readline()
-    tup = eval(line)
-    name_1 = profile[tup[0]]
-    name_2 = profile[tup[1]]
-
-    for k, v in class_info.iteritems():
-        if name_1 in v and name_2 in v:
-            hit_num += 1
-            break
-    for point in break_point:
-        if i == point/2 - 1:
-            rate = hit_num / (i+1)
-            outfile.write(str(point/2) + ':' + str(rate) + '\n')
+# for i in range(0, 205170):
+#     line = net_file.readline()
+#     tup = eval(line)
+#     name_1 = profile[tup[0]]
+#     name_2 = profile[tup[1]]
+#
+#     for k, v in class_info.iteritems():
+#         if name_1 in v and name_2 in v:
+#             hit_num += 1
+#             break
+#     for point in break_point:
+#         if i == point/2 - 1:
+#             rate = hit_num / (i+1)
+#             outfile.write(str(point/2) + ':' + str(rate) + '\n')
